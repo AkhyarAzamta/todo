f7314caf92bc0ce8107560288b7d0e53f7c865d4
Akhyar Azamta <bebyraska14.com>
2022-12-26 16:40:51 +0000

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <h1>Hello Word!</h1>
+    <div class="container">
+  
+    </div>
+  );
+}
+
+export default App;
